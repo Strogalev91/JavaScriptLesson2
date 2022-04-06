b9b654b0078028fb5ad80df965ae3b93ca149147
Strogalev Ilia <74052206+Strogalev91@users.noreply.github.com>
2022-04-06 23:42:59 +0500

    lesson#2 v1.0

---
+++ main.js
@@ -0,0 +1,48 @@
+//Task#1
+for (let i = 10; i <= 50; i += 2){
+	console.log(i);
+}
+
+//Task#2
+const myself = {
+	firstName : "Ilia",
+	lastName : "Strogalev",
+	age : 30,
+	pets : false
+};
+console.log(myself);
+
+//Task#3
+const array = [
+	'я в Симбирск, ',
+	'в трактире. ',
+	'с утра',
+	'В ту же ночь',
+  	'Я остановился',
+  	'для закупки', 
+  	'что и было поручено Савельичу.',
+  	'приехал',
+  	'где должен был',
+  	'нужных вещей,',
+  	'отправился по лавкам',
+  	'пробыть сутки',
+	'Савельич'
+];
+const correctSequence = [3, 7, 0, 8, 11, 5, 9, 6, 4, 1, 12, 2, 10];
+const unitedStrings = correctSequence.reduce((unitedStrings, number) => [...unitedStrings, array[number]], []);
+const result = unitedStrings.join (' ');
+console.log(result);
+
+//Task#4
+const name = (firstName, lastName) => {
+	const fullName = `${firstName} ${lastName}`;
+	console.log(fullName);
+};
+name('Ilia', 'Strogalev');
+
+//Task#5
+let oddNumbers = 21;
+while(oddNumbers <= 67){
+	console.log(oddNumbers);
+	oddNumbers += 2;
+}
